6e129a7e581a3690f1346f26be7257d14a94f181
mpottebaum <mpottebaum@gmail.com>
2020-05-20 15:34:11 -0500

    built retrieve action on games controller for page refreshes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,29 @@ class GamesController < ApplicationController
         render json: game
     end
 
+    def retrieve
+        game = Game.find(params[:id])
+
+        serialized_game = ActiveModelSerializers::Adapter::Json.new(
+            GameSerializer.new(game)
+        ).serializable_hash
+
+        neighborhoods = Neighborhood.all
+        serialized_neighborhoods = Neighborhood.all.map do |neighborhood|
+            serialized_neighborhood = ActiveModelSerializers::Adapter::Json.new(
+                NeighborhoodSerializer.new(neighborhood)
+            ).serializable_hash
+            serialized_neighborhood[:neighborhood]
+        end
+        zones = Zone.all
+
+        render json: {
+            game: serialized_game[:game],
+            neighborhoods: serialized_neighborhoods,
+            zones: zones
+        }
+    end
+
     def fortify
         game = Game.find(params[:id])
         game.update(turn_stage: 2)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :games
+  get '/games/:id/retrieve', to: 'games#retrieve'
   patch '/games/:id/fortify', to: 'games#fortify'
   resources :players do
     resources :militia
