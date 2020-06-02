52dafb8561e9db61b0a011c8877f7e3edbaf02c6
mpottebaum <mpottebaum@gmail.com>
2020-06-02 13:08:00 -0500

    adding mount path to environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://whispering-crag-66572.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ '*' ]
 
